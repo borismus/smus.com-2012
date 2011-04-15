3b4b5dda895a527dfe79fe2d2e2b2bd46c91eb24
Boris Smus <boris@borismus.com>
2011-04-15 00:57:59 -0700

    Added tons of files

---
+++ media/js/base.js
@@ -0,0 +1,29 @@
+$(function() {
+
+  // Enable navbar funkiness
+  $('nav .icons img').mouseover(function() {
+    var className = $(this).attr('class');
+    console.log('hover on img. className ' + className);
+    // Hide all messages, only show the one corresponding to the className
+    $('nav .message > *').hide()
+    $('nav .message .' + className).show();
+  });
+
+  // Make the navbar persist if we scroll far enough
+  var offsetTop = $('nav').offset().top;
+  console.log(offsetTop);
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > offsetTop) {
+      $('nav').addClass('fix');
+    } else {
+      $('nav').removeClass('fix');
+    }
+  });
+
+  // Format relative dates
+  $('.date .relative').timeago();
+
+  // Highlight code
+  hljs.tabReplace = '    ';
+  hljs.initHighlightingOnLoad();
+});
